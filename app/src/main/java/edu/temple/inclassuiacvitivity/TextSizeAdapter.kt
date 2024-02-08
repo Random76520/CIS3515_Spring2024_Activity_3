16492bea4051547f82cc30402b6d58af74eb7435
Jonathan Augustin <tun45218@temple.edu>
2024-02-08 11:36:16 -0500

    Functions in TextSizeAdapter are populated

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,33 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _numbers: Array<String>) : BaseAdapter() {
 
+    private val context = _context
+    private val numbers = _numbers
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numbers.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numbers[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textview = TextView(context)
+        textview.text = numbers[position]
+        textview.textSize = 22f
+        textview.setPadding(5, 0, 0, 10)
+        return textview
     }
 
 }
